e6d21ca31a1b06e40eeda575bdbc6166e2c9b19a
joposada7 <joposada7@gmail.com>
2023-04-17 07:37:53 -0400

    restructure safety controller

--- src/safety_controller.py
+++ src/safety_controller.py
@@ -4,90 +4,92 @@ import numpy as np
 import message_filters
 
 import rospy
-from rospy.numpy_msg import numpy_msg
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
 from visualization_tools import *
 import math
 
-def safety_control(ackermann, lidar_data, pub):
-    if lidar_data.angle_increment == 0.0:
-        return  # Early return for uninitialized LaserScan
+class SafetyController:
+    """
+    Processes desired car driving commands, and stops the car if about to crash.
+        - Takes into account car size (HALF_CAR_WIDTH)
+        - Predicts the next few seconds (LOOK_AHEAD_TIME)
+        - Predicts car's future trajectory based on current speed and turning angle
+    """
+    LOOK_AHEAD_TIME = rospy.get_param("look_ahead_time", 0.5) # seconds
+    HALF_CAR_WIDTH = rospy.get_param("half_car_width", 0.2) # meters
+    IGNORE_CLOSE_DIST = 0.1 # meters
 
-    # how far in seconds do we want to check
-    look_ahead_time = rospy.get_param("look_ahead_time")
-    half_car_width = rospy.get_param("half_car_width")
+    def __init__(self):
+        self.lidar_sub = message_filters.Subscriber("/scan", LaserScan)
+        self.ackermann_sub = message_filters.Subscriber("/vesc/high_level/ackermann_cmd_mux/output", AckermannDriveStamped)
 
-    # we only want from -90 deg to 90 deg
-    low_index = int(
-        (-(math.pi/2.0) - lidar_data.angle_min)/lidar_data.angle_increment)
-    high_index = int(
-        ((math.pi/2.0) - lidar_data.angle_min)/lidar_data.angle_increment)
+        # Initialize safety control publisher
+        self.pub = rospy.Publisher("/vesc/low_level/ackermann_cmd_mux/input/safety", AckermannDriveStamped, queue_size=1)
+        ts = message_filters.ApproximateTimeSynchronizer([self.lidar_sub, self.ackermann_sub], 10, 0.05)
+        ts.registerCallback(self.safety_control)
 
-    ranges = np.array(lidar_data.ranges)[low_index:high_index]
-    angles = np.arange(lidar_data.angle_min,
-                       lidar_data.angle_max, lidar_data.angle_increment)[low_index:high_index]
+    def safety_control(self, lidar_msg, ackermann_msg):
+        if lidar_msg.angle_increment == 0.0:
+            # Early return for uninitialized LaserScan
+            return
 
-    # if we are turning left we have to multiply by -1 in places
-    # find the radius the car is turning around
-    if ackermann.drive.steering_angle == 0.0:
-        turning_right = 1.0
-        turning_radius = 1000.0
-    elif ackermann.drive.steering_angle < 0.0:
-        turning_right = 1.0
-        turning_radius = abs(
-            0.35 / np.tan(ackermann.drive.steering_angle))
-    else:
-        turning_right = -1.0
-        turning_radius = abs(
-            0.35 / np.tan(ackermann.drive.steering_angle))
+        # We only care about forward FOV of car (-90 to 90 deg)
+        low_index = get_range_index(np.deg2rad(-90), lidar_msg)
+        high_index = get_range_index(np.deg2rad(90), lidar_msg)
 
-    max_angle = (look_ahead_time * ackermann.drive.speed) / turning_radius
+        ranges = np.array(lidar_msg.ranges)[low_index:high_index]
+        angles = np.arange(lidar_msg.angle_min, lidar_msg.angle_max, lidar_msg.angle_increment)
+        angles = angles[low_index:high_index]
 
-    # take the coordinates from the lidar's perspective and find what they are from the turning radius point
-    # the angle is relative to our turning radius point to the lidar, positive if ahead of the car
-    # then see if there are any points in the car's path, and if so stop the car
-    for index, range in enumerate(ranges):
-        if range < 0.10:
-            continue
+        # Extract turning radius and direction
+        if ackermann_msg.drive.steering_angle == 0.0:
+            turning_right = 1
+            turning_radius = 1000.0 # ~inf
+        elif ackermann_msg.drive.steering_angle < 0.0:
+            turning_right = 1
+            turning_radius = abs(0.35 / np.tan(ackermann.drive.steering_angle))
+        else:
+            turning_right = -1
+            turning_radius = abs(0.35 / np.tan(ackermann.drive.steering_angle))
 
-        inner_angle = (math.pi/2.0) - (turning_right * angles[index])
+        max_angle = (self.LOOK_AHEAD_TIME * ackermann.drive.speed) / turning_radius
 
-        dist = math.sqrt(range**2.0 + turning_radius**2.0 -
-                         (2.0 * range * turning_radius * np.cos(inner_angle)))
+        """
+        Take coordinates from the LIDAR's perspective, find trajectory of car.
+        """
+        for index, range in enumerate(ranges):
+            if range < self.IGNORE_CLOSE_DIST:
+                # Filter out erroneous LIDAR points
+                continue
 
-        angle = np.arcsin((range/dist) * np.sin(inner_angle))
+            inner_angle = np.deg2rad(90) - (turning_right * angles[index])
 
-        if dist > turning_radius - half_car_width and dist < turning_radius + half_car_width and angle < max_angle:
-            rospy.loginfo("Safety Controller engaged!")
+            dist = math.sqrt(range**2.0 + turning_radius**2.0 - (2.0*range*turning_radius*np.cos(inner_angle)))
+            angle = np.arcsin((range/dist) * np.sin(inner_angle))
 
-            msg = AckermannDriveStamped()
-            msg.header.stamp = rospy.Time.now()
-            msg.header.frame_id = "base_link"
+            if dist > (turning_radius - half_car_width) and dist < (turning_radius + half_car_width) and angle < max_angle:
+                rospy.loginfo("Safety Controller engaged!")
 
-            msg.drive.speed = 0
+                msg = AckermannDriveStamped()
+                msg.header.stamp = rospy.Time.now()
+                msg.header.frame_id = "base_link"
 
-            msg.drive.steering_angle_velocity = 0.0
-            msg.drive.acceleration = 0.0
-            msg.drive.jerk = 0.0
+                msg.drive.speed = 0
 
-            pub.publish(msg)
-            break
+                msg.drive.steering_angle_velocity = 0.0
+                msg.drive.acceleration = 0.0
+                msg.drive.jerk = 0.0
+
+                self.pub.publish(msg)
+                return
+
+    def get_range_index(angle, lidar_msg):
+        return int((angle - lidar_msg.angle_min)/lidar_msg.angle_increment)
 
 
 if __name__ == "__main__":
     rospy.init_node('safety_controller')
     rospy.Rate(20)  # 20hz
-
-    ackermann_sub = message_filters.Subscriber(
-        "/vesc/high_level/ackermann_cmd_mux/output", AckermannDriveStamped)
-    lidar_sub = message_filters.Subscriber("/scan", LaserScan)
-
-    pub = rospy.Publisher(
-        "/vesc/low_level/ackermann_cmd_mux/input/safety", AckermannDriveStamped, queue_size=1)
-
-    ts = message_filters.ApproximateTimeSynchronizer(
-        [ackermann_sub, lidar_sub], 10, 0.05)
-    ts.registerCallback(safety_control, pub)
-
+    safety_controller = SafetyController()
     rospy.spin()
